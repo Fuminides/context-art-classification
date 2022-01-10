2e13aae0642ec6e226581133003fba5740930313
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2022-01-10 13:17:51 +0100

    Update launch_school_fcm.sh

--- launch_school_fcm.sh
+++ launch_school_fcm.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #$ -N Semart_FCM_school
 #$ -q cal.q
-#$ -cwd
-#$ -t 1
 #$ -e salidas/school_error.txt
 #$ -o salidas/school_out.txt
 source activate py365
+
 python main.py --mode train --model kgm --att school --dir_dataset "../SemArt/" --batch_size 126 --nepochs 300 --embedds fcm
