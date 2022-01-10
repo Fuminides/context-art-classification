0378d27524884382b18ff1b5770389773bbc3642
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2022-01-10 13:28:45 +0100

    Fixed launch sripts

--- launch_time_fcm.sh
+++ launch_time_fcm.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-#$ -N Semart_FCM_school
 #$ -q cal.q
 #$ -cwd
-#$ -t 1
-#$ -e salidas/school_error.txt
-#$ -o salidas/school_out.txt
 source activate py365
-python main.py --mode train --model kgm --att time --dir_dataset "../SemArt/" --batch_size 126 --nepochs 300 --embedds fcm
+
+python main.py --mode train --model kgm --att time --dir_dataset "../SemArt/" --batch_size 126 --nepochs 300 --embedds fcm >salidas/time_out.txt 2>salidas/time_error.txt
--- launch_type_fcm.sh
+++ launch_type_fcm.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-#$ -N Semart_FCM_school
 #$ -q cal.q
 #$ -cwd
-#$ -t 1
-#$ -e salidas/school_error.txt
-#$ -o salidas/school_out.txt
 source activate py365
 
-python main.py --mode train --model kgm --att type --dir_dataset "../SemArt/" --batch_size 126 --nepochs 300 --embedds fcm
+python main.py --mode train --model kgm --att type --dir_dataset "../SemArt/" --batch_size 126 --nepochs 300 --embedds fcm >salidas/type_out.txt 2>salidas/type_error.txt
