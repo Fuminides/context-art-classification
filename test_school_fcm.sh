8f21facce05829d78a3a7a5e5ff2ace2dd27964a
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2022-01-12 13:04:35 +0100

    Test commands

---
+++ test_school_fcm.sh
@@ -0,0 +1,1 @@
+python main.py --att school  --mode test --model kgm --embedds fcm --model_path Models/kgm-author/fcm_school_best_model.pth.tar --dir_dataset "../SemArt/"
---
+++ test_time_fcm.sh
@@ -0,0 +1,1 @@
+python main.py --att time --mode test --model kgm --embedds fcm --model_path Models/kgm-author/fcm_time_best_model.pth.tar --dir_dataset "../SemArt/"
