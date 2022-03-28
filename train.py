b3b178801c43d3a495ef1ef16a00c98ea2768e46
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2022-03-28 12:10:53 +0200

    fix

--- train.py
+++ train.py
@@ -455,13 +455,9 @@ def train_gcn_classifier(args_dict):
     # Use the kgs to generate a sparse matrix
     total_edge_list = pd.concat([train_edge_list, val_edge_list, test_edge_list], axis=0)
     n_samples = int(total_edge_list.max().max()+1)
-    '''adj_sparse = dok_matrix((n_samples, n_samples), dtype=np.int8)
-    for row in range(train_edge_list.shape[0]):
-        emisor = total_edge_list.iloc[row, 0]
-        receptor = total_edge_list.iloc[row, 1]
-        adj_sparse[emisor, receptor] = 1'''
+   
 
-    tensor_total_edge_list = torch.tensor(np.array(total_edge_list), dtype=torch.long)
+    tensor_total_edge_list = torch.tensor(np.array(total_edge_list).T, dtype=torch.long)
     
 
     # Load the feature matrix from the vis+node2vec representations
@@ -484,9 +480,14 @@ def train_gcn_classifier(args_dict):
     test_mask = np.array([0] * n_samples)
     test_mask[-test_feature_matrix.shape[0]:] = 1
     test_mask = torch.tensor(test_mask, dtype=torch.uint8)
+   
+    if torch.cuda.is_available():
+        total_samples = total_samples.cuda()
+        tensor_total_edge_list = tensor_total_edge_list.cuda()
+        train_mask = train_mask.cuda()
+        val_mask = val_mask.cuda()
+        test_mask = test_mask.cuda()
 
-    #Load labels
-    
     #Load all the data as Data object for pytorch geometric
     data = Data(x=total_samples, edge_index=tensor_total_edge_list)
     data.train_mask = train_mask
