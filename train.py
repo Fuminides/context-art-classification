4613c333adf7ba56aa36d43577d7b3601d3b22bd
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2022-05-11 12:48:04 +0200

    Update train.py

--- train.py
+++ train.py
@@ -142,7 +142,7 @@ def trainEpoch(args_dict, train_loader, model, criterion, optimizer, epoch, extr
                     train_loss = criterion[0](output, target_var[0].long())
                     
                 else:
-                    class_loss = criterion[0](output, target_var[0].long())
+                    class_loss = criterion[0](output[0], target_var[0].long())
                     encoder_loss = criterion[1](output[1], target_var[1].long())
 
                     train_loss = args_dict.lambda_c * class_loss + \
