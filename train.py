9bb754e919e4cb7e2def9fc6869d5fbf4a73b79c
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2022-05-11 12:34:04 +0200

    Update train.py

--- train.py
+++ train.py
@@ -323,7 +323,7 @@ def train_knowledgegraph_classifier(args_dict):
         else:
             model = KGM_append(len(att2i))
     else:
-        if args_dict.append == 'append':
+        if args_dict.append != 'append':
             model = KGM(len(att2i), end_dim=N_CLUSTERS)
         else:
             model = KGM_append(len(att2i))
