adb6e34511ad46e1f3cd433618b8133fdd7c1ae1
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2022-05-11 16:01:34 +0200

    Update text_encoding.py

--- text_encoding.py
+++ text_encoding.py
@@ -56,9 +56,9 @@ def tf_idf_load_train_text_corpus(semart_path='../SemArt/', k=10, append='append
     vectorizer.fit(corpus)
     print(vectorizer.transform(corpus).shape, bool_freqs.shape)
     
-    chosen_coded_semart_train = fcm_coded_context(vectorizer.transform(corpus)[:, bool_freqs], clusters=128)
-    chosen_coded_semart_val = fcm_coded_context(vectorizer.transform(val_corpus)[:, bool_freqs], clusters=128)
-    chosen_coded_semart_test = fcm_coded_context(vectorizer.transform(test_corpus)[:, bool_freqs], clusters=128)
+    chosen_coded_semart_train = fcm_coded_context(vectorizer.transform(corpus)[:, bool_freqs.squeeze()], clusters=128)
+    chosen_coded_semart_val = fcm_coded_context(vectorizer.transform(val_corpus)[:, bool_freqs.squeeze()], clusters=128)
+    chosen_coded_semart_test = fcm_coded_context(vectorizer.transform(test_corpus)[:, bool_freqs.squeeze()], clusters=128)
     
     if append != 'append':
         return chosen_coded_semart_train
