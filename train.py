13bd612e32783ddca09258672b32bc3ce0b429cc
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2022-06-06 13:01:48 +0200

    Fix MTL

--- train.py
+++ train.py
@@ -310,11 +310,11 @@ def valEpoch(args_dict, val_loader, model, criterion, epoch):
     # Return acc
     return acc
 
-def multi_class_loss(criterion, target_var, output, index_loss):
-    return 0.25 * criterion(output[0][index_loss], target_var[0][index_loss].long()) + \
-                            0.25 * criterion(output[1][index_loss], target_var[1][index_loss].long()) + \
-                            0.25 * criterion(output[2][index_loss], target_var[2][index_loss].long()) + \
-                            0.25 * criterion(output[3][index_loss], target_var[3][index_loss].long())
+def multi_class_loss(criterion, target_var, output):
+    return 0.25 * criterion(output[0], target_var[0].long()) + \
+                            0.25 * criterion(output[1], target_var[1].long()) + \
+                            0.25 * criterion(output[2], target_var[2].long()) + \
+                            0.25 * criterion(output[3], target_var[3].long())
 
 
 def train_knowledgegraph_classifier(args_dict):
