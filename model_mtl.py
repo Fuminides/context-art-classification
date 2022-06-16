d9ff6efffd8eee15b3b8cedcd4b7570e480c7392
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2022-06-16 12:56:10 +0200

    Update model_mtl.py

--- model_mtl.py
+++ model_mtl.py
@@ -1,20 +1,25 @@
 import torch.nn as nn
 from torchvision import models
 import torch
+import clip
 
 class MTL(nn.Module):
     # Inputs an image and ouputs the predictions for each classification task
 
     def __init__(self, num_class, model='resnet'):
         super(MTL, self).__init__()
-
+        self.model = model
         # Load pre-trained visual model
         if model == 'resnet':
             resnet = models.resnet50(pretrained=True)
             embedding_size = 2048
-        elif 'vgg':
+        elif model == 'vgg':
             resnet = models.vgg16(pretrained=True)
             embedding_size = 25088
+        elif model == 'clip':
+            resnet, _ = clip.load("ViT-B/32")
+            embedding_size = 512
+
 
         self.resnet = nn.Sequential(*list(resnet.children())[:-1])
             
@@ -27,7 +32,11 @@ class MTL(nn.Module):
 
     def forward(self, img):
 
-        visual_emb = self.resnet(img)
+        if self.mode != 'clip':
+            visual_emb = self.resnet(img)
+        else:
+            visual_emb = self.resnet.encode_image(img)
+
         visual_emb = visual_emb.view(visual_emb.size(0), -1)
         out_type = self.class_type(visual_emb)
         out_school = self.class_school(visual_emb)
