0b52572f7220adb0790e7d64b763d8b3576f9f90
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2022-06-23 13:12:13 +0200

    Fuzzy rules embeds fix

--- fuzzy_rules.R
+++ fuzzy_rules.R
@@ -1,7 +1,27 @@
 main <- function(X, val, test) {
-  rb <- searchrule(X,
-                   lhs=which(vars(d) != "y"),
-                   rhs=which(vars(d) == "uptake"),
+  fuzzy_X = lcut(X)
+  fuzzy_val = lcut(val)
+  fuzzy_test = lcut(test)
+  
+  rb = searchrules(fuzzy_X,
+                   lhs=which(vars(fuzzy_X) != "y"),
+                   rhs=which(vars(fuzzy_X) == "y"),
                    minConfidence=0.5)
-  pbld(testing, rb, p, v, type="global")
+  
+  train_consequents = fire(fuzzy_X, rb)
+  val_consequents = fire(fuzzy_val, rb)
+  test_consequents = fire(fuzzy_test, rb)
+  
+  lhsupport = rb[[2]][,2]
+  best_rule = which.max(lhsupport)
+  
+  train_consequents = train_consequents[[best_rule]]
+  val_consequents = train_consequents[[best_rule]]
+  test_consequents = train_consequents[[best_rule]]
+
+  df <- data.frame(train_consequents, val_consequents, test_consequents)
+  
+  return(df)
 }
+
+main(xiris, xiris, xiris)
--- fuzzy_rules_clustering.py
+++ fuzzy_rules_clustering.py
@@ -1,25 +1,31 @@
-semart_train_path = ''
-semart_val_path = ''
-semart_test_path = ''
+semart_train_path = 'tfidf_semart_train.csv'
+semart_val_path = 'tfidf_semart_val.csv'
+semart_test_path = 'tfidf_semart_test.csv'
 
 import numpy as np
 import pandas as pd
 
 
 def compute_q(X):
-    centroid = np.mean(X) #I interpreted right? God knows
-    return np.sum((X - centroid)**2, axis=-1)
+    centroid = np.mean(X, axis=0) #I interpreted right? God knows
+    return np.sum((X - centroid)**2, axis=None)
 
 def best_rule(rules_antecedents):
     return np.argmax(np.prod(rules_antecedents, axis=1))
 
-def compute_rules_output(X, y):
+def compute_rules_output(X, X_val, X_test, y):
     import rpy2.robjects as robjects
     from rpy2.robjects import pandas2ri
 
     names = [str(x) for x in range(X.shape[0])]
     X_df = pd.DataFrame(X, columns=names)
     X_df['y'] = y
+
+    X_val_df = pd.DataFrame(X_val, columns=names)
+    X_val_df['y'] = y
+
+    X_test_df = pd.DataFrame(X_test, columns=names)
+    X_test_df['y'] = y
 
     # Defining the R script and loading the instance in Python
     r = robjects.r
@@ -30,25 +36,32 @@ def compute_rules_output(X, y):
     #converting it into r object for passing into r function
     df_r = pandas2ri.ri2py(X_df)
     #Invoking the R function and getting the result
-    df_result_r = filter_country_function_r(df_r)
+    df_result_r = filter_country_function_r(df_r, X_val_df, X_test_df)
     #Converting it back to a pandas dataframe.
     df_result = pandas2ri.py2ri(df_result_r)
 
-    antecedents = df_result.iloc[:, :-1].values
-    consequents = df_result.iloc[:, -1].values
+    consequents_train = df_result.iloc[:, -3].values
+    consequents_val = df_result.iloc[:, -2].values
+    consequents_test = df_result.iloc[:, -1].values
 
-    return antecedents, consequents
+    return consequents_train, consequents_val, consequents_test 
 
-def frbc(X, output_clusters=128):
+def frbc(X, X_val, X_test, output_clusters=128):
     m_N = X.shape[0]
+    m_N_val = X_val.shape[0]
+    m_N_test = X_test.shape[0]
+
     y = [0] * m_N
     
     q = 0
     q_prima = 0
     
-    final_memberships = np.zeros(m_N, 128)
+    final_memberships = np.zeros((m_N, 128))
+    final_memberships_val = np.zeros((m_N_val, 128))
+    final_memberships_test = np.zeros((m_N_test, 128))
 
     for j in range(output_clusters):
+        print('Rule: ', j)
         artificial_samples = []
         while q >= q_prima:
             artificial_sample = np.random.random_sample((X.shape[1]))
@@ -58,17 +71,29 @@ def frbc(X, output_clusters=128):
             y.append(1)
 
             q = compute_q(X)
-            q_prima = compute_q(artificial_samples)
+            q_prima = compute_q(np.array(artificial_samples))
         
         # Generate rules
-        antecedents, consequents = compute_rules_output(X, np.array(y))
+        consequents_train, consequents_val, consequents_test = compute_rules_output(X, np.array(y))
 
-        best_rule = best_rule(antecedents)
-        select = consequents < 0.5
+        select = consequents_train < 0.5
 
-        final_memberships[j, :] = consequents[:, best_rule]
+        final_memberships[j, :] = consequents_train
+        final_memberships_val[j, :] = consequents_val
+        consequents_test[j, :] = consequents_test
         X = X[select, :]
         y = [z for ix, z in enumerate(y) if select[ix]]
     
-    return final_memberships
+    return final_memberships, final_memberships_val, final_memberships_test
 
+if __name__ == '__main__':
+    # Load data
+    X_train = pd.read_csv(semart_train_path).values
+    X_val = pd.read_csv(semart_val_path).values
+    X_test = pd.read_csv(semart_test_path).values
+
+    # Run FRBC
+    final_memberships, final_memberships_val, final_memberships_test = frbc(X_train, X_val, X_test, output_clusters=128)
+    final_memberships.to_csv('rule_embds_train.csv')
+    final_memberships_val.to_csv('rule_embds_val.csv')
+    final_memberships_test.to_csv('rule_embds_test.csv')
