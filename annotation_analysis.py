3520782f65649c3fb09399c537248d704a7baad8
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2022-10-10 17:52:09 +0100

    Update annotation_analysis.py

--- annotation_analysis.py
+++ annotation_analysis.py
@@ -456,7 +456,8 @@ class Gallery:
 
 if __name__ == '__main__':
     symbol_context, paintings_names, symbols_names = __load_semart_proxy(mode='train')
-    semart_Gallery = Gallery(symbols_names, paintings_names, symbol_context, '../SemArt/')
+    semart_Gallery = Gallery(symbols_names, paintings_names, symbol_context, '../../SemArt/')
+    semart_Gallery.most_repeated_symbols(100)
 
     #res = symbol_report(symbol_context, symbol_names=symbols_names, painting_names=paintings_names)
     res = semart_gen_symbol_graph(symbol_context)
