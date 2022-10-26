ef34add80a88c48c19febe49ba13c9b520677086
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2022-10-26 15:27:27 +0100

    Update train.py

--- train.py
+++ train.py
@@ -139,7 +139,7 @@ def trainEpoch(args_dict, train_loader, model, criterion, optimizer, epoch, symb
                 print('Saving features...')
                 feat = model.features(input_var[0])
 
-                pd.DataFrame(feat.cpu().numpy()).to_csv('./DeepFeatures/train_x_' + str(batch_idx) + '.csv')
+                pd.DataFrame(feat.data.cpu().numpy()).to_csv('./DeepFeatures/train_x_' + str(batch_idx) + '.csv')
                 pd.DataFrame(target_var[0].cpu().numpy()).to_csv('./DeepFeatures/train_y_' + str(batch_idx) + '.csv')
             
             actual_index += args_dict.batch_size
