dd652dd92af2ebe29aeef751af4ef59abdfead9a
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2022-11-04 16:53:53 +0000

    Update intel_sesion_jobs.sh

--- intel_sesion_jobs.sh
+++ intel_sesion_jobs.sh
@@ -3,7 +3,7 @@
 source ~/anaconda3Intel/etc/profile.d/conda.sh
 conda activate py39
 
-tsp -S 14
+tsp -S 4
 # python --mode train --workers 0 --model kgm --append gradient --att author --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds bow --clusters 128 --k 100 # Already done
 # python --mode train --workers 0 --model kgm --append gradient --att school --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds bow --clusters 128 --k 100 # Already done
 python main.py --mode train --workers 0 --model kgm --append gradient --att time   --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds bow --clusters 128 --k 100
