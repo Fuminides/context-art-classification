81304d2e1a7a19bb2323332e2df3245f17e253bb
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2022-11-04 16:00:42 +0000

    Create intel_sesion_jobs.sh

---
+++ intel_sesion_jobs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source ~/anaconda3Intel/etc/profile.d/conda.sh
+conda activate py39
+
+tsp -S 14
+# python --mode train --workers 0 --model kgm --append gradient --att author --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds bow --clusters 128 --k 100 # Already done
+# python --mode train --workers 0 --model kgm --append gradient --att school --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds bow --clusters 128 --k 100 # Already done
+python main.py--mode train --workers 0 --model kgm --append gradient --att time   --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds bow --clusters 128 --k 100
+python main.py--mode train --workers 0 --model kgm --append gradient --att type   --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds bow --clusters 128 --k 100
+
+python main.py--mode train --workers 0 --model kgm --append gradient --att author --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds clip --clusters 128 --k 100
+python main.py--mode train --workers 0 --model kgm --append gradient --att school --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds clip --clusters 128 --k 100
+python main.py--mode train --workers 0 --model kgm --append gradient --att time   --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds clip --clusters 128 --k 100
+python main.py--mode train --workers 0 --model kgm --append gradient --att type   --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds clip --clusters 128 --k 100
+
+python main.py--mode train --workers 0 --model kgm --append gradient --att author --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds graph --clusters 128 --k 100
+python main.py--mode train --workers 0 --model kgm --append gradient --att school --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds graph --clusters 128 --k 100
+python main.py--mode train --workers 0 --model kgm --append gradient --att time   --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds graph --clusters 128 --k 100
+python main.py--mode train --workers 0 --model kgm --append gradient --att type   --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds graph --clusters 128 --k 100
+
+python main.py--mode train --workers 0 --model mtl --append gradient --att author --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds graph --clusters 128 --k 100
+python main.py--mode train --workers 0 --model mtl --append gradient --att school --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds graph --clusters 128 --k 100
+python main.py--mode train --workers 0 --model mtl --append gradient --att time   --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds graph --clusters 128 --k 100
+python main.py--mode train --workers 0 --model mtl --append gradient --att type   --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds graph --clusters 128 --k 100
