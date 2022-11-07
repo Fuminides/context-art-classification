9d9d98a6df4354ff6c2253a2378cc92335f3a205
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2022-11-07 12:06:35 +0000

    Update intel_sesion_jobs.sh

--- intel_sesion_jobs.sh
+++ intel_sesion_jobs.sh
@@ -6,8 +6,8 @@ conda activate py39
 tsp -S 4
 # python --mode train --workers 0 --model kgm --append gradient --att author --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds bow --clusters 128 --k 100 # Already done
 # python --mode train --workers 0 --model kgm --append gradient --att school --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds bow --clusters 128 --k 100 # Already done
-python main.py --mode train --workers 0 --model kgm --append gradient --att time   --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds bow --clusters 128 --k 100
-CUDA_VISIBLE_DEVICES=1 python main.py --mode train --workers 0 --model kgm --append gradient --att type   --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds bow --clusters 128 --k 100
+#python main.py --mode train --workers 0 --model kgm --append gradient --att time   --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds bow --clusters 128 --k 100
+#CUDA_VISIBLE_DEVICES=1 python main.py --mode train --workers 0 --model kgm --append gradient --att type   --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds bow --clusters 128 --k 100
  
 python main.py --mode train --workers 0 --model kgm --append gradient --att author --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds clip --clusters 128 --k 100
 CUDA_VISIBLE_DEVICES=1 python main.py --mode train --workers 0 --model kgm --append gradient --att school --dir_dataset ../SemArt/ --batch_size 128 --nepochs 300 --embedds clip --clusters 128 --k 100
