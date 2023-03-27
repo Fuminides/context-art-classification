15c35ac629d5bf54c1980e1c56b3f7b7fad07731
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2023-03-27 17:22:46 +0200

    Update dataloader_sym.py

--- Symbol task/dataloader_sym.py
+++ Symbol task/dataloader_sym.py
@@ -43,7 +43,7 @@ class ArtDatasetSym(data.Dataset):
         self.transform = transform
 
         self.imageurls = list(df['IMAGE_FILE'])
-        fiability_path = os.path.join(args_dict.dir_dataset, 'fiabilities_' + self.set + '.csv')
+        fiability_path = os.path.join('Data/', 'fiabilities_' + self.set + '.csv')
         self.fiability =pd.read_csv(fiability_path, index_col=0)
 
 
