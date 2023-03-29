dfc8111e6414345c56e94976261aa1efab838b43
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2023-03-29 18:16:44 +0200

    Rule data prepared

--- Infus_experiments_rules_data/Rule_data_preparation.py
+++ Infus_experiments_rules_data/Rule_data_preparation.py
@@ -1,18 +1,32 @@
+'''
+Use this script to prepare the data for the rules experiments.
+
+author: @fuminides (Javier Fumanal Idocin)
+'''
 import pandas as pd
 from scipy.special import softmax
 
-style_train = softmax(pd.read_csv('Infus_experiments_rules_data/style_predictions_val.csv', index_col=0), axis=1)
-style_val = softmax(pd.read_csv('Infus_experiments_rules_data/style_predictions_train.csv', index_col=0), axis=1)
-style_test = softmax(pd.read_csv('Infus_experiments_rules_data/style_predictions_test.csv', index_col=0), axis=1)
+style_train = pd.read_csv('Infus_experiments_rules_data/style_predictions_val.csv', index_col=0)
+style_val = pd.read_csv('Infus_experiments_rules_data/style_predictions_train.csv', index_col=0)
+style_test = pd.read_csv('Infus_experiments_rules_data/style_predictions_test.csv', index_col=0)
 
-grad_train = pd.read_csv('Infus_experiments_rules_data/train_grad_features.csv', index_col=0)
-grad_train.columns = ['Small', 'Large', 'One or two', 'Few', 'Some', 'Lots']
+grad_train = pd.read_csv('Infus_experiments_rules_data/GradCam_feats.csv', index_col='Image')
+grad_train.drop('Unnamed: 0', axis=1, inplace=True)
+grad_train.index = grad_train.index.str.replace('.csv', '')
+# Erase the elements in grad_train that are not in style_train
+grad_train = grad_train.loc[style_train.index]
 
-grad_train[['Small', 'Large']]
+aux = pd.concat([style_train, grad_train], axis=1, ignore_index=False)
 
-aux = pd.concat([style_train, grad_train], axis=1)
+deep_features_full = pd.read_csv('Infus_experiments_rules_data/train_deep_features.csv', index_col=0)
 
-grad_train[['Small', 'Large']] = softmax(grad_train[['Small', 'Large']], axis=1)
-grad_train[['One or two', 'Few', 'Some', 'Lots']] = softmax(grad_train[['One or two', 'Few', 'Some', 'Lots']], axis=1)
+# deep_features_full = pd.concat([deep_features_train, deep_features_val], axis=0, ignore_index=False)
+deep_features_full = deep_features_full.loc[aux.index]
+
+
+bigger_than_average = deep_features_full.values.argmax(axis=1)
+aux['Dominant_feature'] = bigger_than_average
+
+aux.to_csv('Infus_experiments_rules_data/explainable_features.csv')
 
 print('Files formated')
