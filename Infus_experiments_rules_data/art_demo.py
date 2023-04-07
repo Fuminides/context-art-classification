f810275f4bf481d7e1f6a41577b590e6dc508634
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2023-04-07 13:40:36 +0200

    Update art_demo.py

--- Infus_experiments_rules_data/art_demo.py
+++ Infus_experiments_rules_data/art_demo.py
@@ -28,25 +28,63 @@ import numpy as np
 import sys
 sys.path.append('/home/javierfumanal/Documents/GitHub/FuzzyT2Tbox/ex_fuzzy/')
 sys.path.append('/home/fcojavier.fernandez/Github/FuzzyT2Tbox/ex_fuzzy/')
+sys.path.append('C:/Users/javier.fumanal/Documents/GitHub/FuzzyT2Tbox/ex_fuzzy/')
 import ex_fuzzy.fuzzy_sets as fs
 import ex_fuzzy.evolutionary_fit as GA
 import ex_fuzzy.utils as utils
 import ex_fuzzy.eval_tools as eval_tools
+import ex_fuzzy.rules as rules
+import ex_fuzzy.eval_rules as evr
 from sklearn.model_selection import train_test_split
 
-from multiprocessing.pool import ThreadPool
-from pymoo.core.problem import StarmapParallelization
+
 # Matthew Correlation Coefficient
 from sklearn.metrics import matthews_corrcoef
 # Supress warnings
 import warnings
 warnings.filterwarnings("ignore")
 
-def load_explainable_features(path='/home/javierfumanal/Documents/GitHub/FuzzyT2Tbox/Demos/occupancy_data/explainable_features.csv', sample_ratio=0.1, feature_studied=0):
+def new_loss(ruleBase: rules.RuleBase, X:np.array, y:np.array, tolerance:float):
+        '''
+        Fitness function for the optimization problem.
+        :param ruleBase: RuleBase object
+        :param X: array of train samples. X shape = (n_samples, n_features)
+        :param y: array of train labels. y shape = (n_samples,)
+        :param tolerance: float. Tolerance for the size evaluation.
+        :return: float. Fitness value.
+        '''
+        # Susbsample X and y to have balanced classes
+        positive_index = np.where(y)[0]
+        negative_index = np.where(y == 0)[0]
+        negative_samples_index = np.random.choice(negative_index, len(positive_index), replace=False)
+        total_index = np.concatenate((positive_index, negative_samples_index))
+        #Shuffle the index
+        np.random.shuffle(total_index)
+        X_balanced = X[total_index]
+        y_balanced = y[total_index]
+        
+        ev_object = evr.evalRuleBase(ruleBase, X_balanced, y_balanced)
+        ev_object.add_rule_weights()
+
+        score_acc = ev_object.classification_eval()
+        score_size = ev_object.size_eval(tolerance)
+
+        alpha = 0.99
+        beta = 1 - alpha
+
+        score = score_acc * alpha + score_size * beta
+
+        return score
+
+
+def load_explainable_features(path='/home/javierfumanal/Documents/GitHub/FuzzyT2Tbox/Demos/occupancy_data/explainable_features.csv', sample_ratio=0.1, feature_studied=0, balance=True):
     try:
         data = pd.read_csv(path, index_col=0).sample(frac=sample_ratio, random_state=33)
     except FileNotFoundError:
-        data = pd.read_csv('/home/fcojavier.fernandez/Github/FuzzyT2Tbox/Demos/occupancy_data/explainable_features.csv', index_col=0).sample(frac=sample_ratio, random_state=33)
+        try:
+            data = pd.read_csv('/home/fcojavier.fernandez/Github/FuzzyT2Tbox/Demos/occupancy_data/explainable_features.csv', index_col=0).sample(frac=sample_ratio, random_state=33)
+        except FileNotFoundError:
+            data = pd.read_csv('C:/Users/javier.fumanal/Documents/GitHub/FuzzyT2Tbox/Demos/occupancy_data/explainable_features.csv', index_col=0).sample(frac=sample_ratio, random_state=33)
     
     data.fillna(0, inplace=True)
     X = data.iloc[:, :-1]
@@ -54,13 +92,15 @@ def load_explainable_features(path='/home/javierfumanal/Documents/GitHub/FuzzyT2
 
     y = y == feature_studied
 
-    # Subsample the dataframe to have balanced classes
-    X = pd.concat([X[y], X[~y].sample(sum(y), random_state=33)], axis=0, ignore_index=False)
-    y = pd.concat([y[y], y[~y].sample(sum(y), random_state=33)], axis=0, ignore_index=False)
+    if balance:
+        # Subsample the dataframe to have balanced classes
+        X = pd.concat([X[y], X[~y].sample(sum(y), random_state=33)], axis=0, ignore_index=False)
+        y = pd.concat([y[y], y[~y].sample(sum(y), random_state=33)], axis=0, ignore_index=False)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=33, stratify=y) #Como que 33?
 
     # Classification with Extreme gradient boosting
+    '''
     from xgboost import XGBClassifier
     model = XGBClassifier()
     model.fit(X_train.values, y_train.values)
@@ -85,7 +125,7 @@ def load_explainable_features(path='/home/javierfumanal/Documents/GitHub/FuzzyT2
     print('Accuracy on train set: ', (y_pred == y_train).mean())
     print('Accuracy on test set: ', (y_pred_test == y_test).mean())
     print()
-
+    '''
     return X, y, X_train, X_test, y_train, y_test
 
 
@@ -94,28 +134,24 @@ try:
     pop_size = int(sys.argv[2])
     nRules = int(sys.argv[3])
     nAnts = int(sys.argv[4])
-    n_threads = int(sys.argv[5])
+    runner = int(sys.argv[5])
     feature_studied = int(sys.argv[6])
 except:
     n_gen = 1000
     pop_size = 30
     nRules = 15
     nAnts = 3
-    n_threads = 1
+    runner = 1
     feature_studied = 19
 
-if n_threads > 1:
-    pool = ThreadPool(n_threads)
-    runner = StarmapParallelization(pool.starmap)
-else:
-    runner = None
 
 fz_type_studied = fs.FUZZY_SETS.t1
-checkpoints = 400
+checkpoints = 0
 
-X, y, X_train, X_test, y_train, y_test = load_explainable_features(sample_ratio=1.0, feature_studied=feature_studied)
+X, y, X_train, X_test, y_train, y_test = load_explainable_features(sample_ratio=1.0, feature_studied=feature_studied, balance=False)
 
 precomputed_partitions = utils.construct_partitions(X, fz_type_studied)
+precomputed_partitions=None
 min_bounds = np.min(X, axis=0).values
 max_bounds = np.max(X, axis=0).values
 domain = [min_bounds, max_bounds]
@@ -124,7 +160,7 @@ print('Training fuzzy classifier:' , nRules, 'rules, ', nAnts, 'ants, ', n_gen, 
 fl_classifier = GA.FuzzyRulesClassifier(nRules=nRules, nAnts=nAnts, 
     linguistic_variables=precomputed_partitions, n_linguist_variables=3, 
     fuzzy_type=fz_type_studied, verbose=True, tolerance=0.0, domain=domain, runner=runner)
-
+fl_classifier.customized_loss(new_loss)
 fl_classifier.fit(X_train, y_train, n_gen=n_gen, pop_size=pop_size, checkpoints=checkpoints)
 
 str_rules = eval_tools.eval_fuzzy_model(fl_classifier, X_train, y_train, X_test, y_test, 
