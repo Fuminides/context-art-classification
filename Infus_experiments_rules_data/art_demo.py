3697349b48eee8f7ca25b2f44c8b5e1a21f171ae
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2023-04-08 12:53:18 +0200

    Update art_demo.py

--- Infus_experiments_rules_data/art_demo.py
+++ Infus_experiments_rules_data/art_demo.py
@@ -65,7 +65,7 @@ def new_loss(ruleBase: rules.RuleBase, X:np.array, y:np.array, tolerance:float):
         X_balanced = X[total_index]
         y_balanced = y[total_index]
         
-        ev_object = evr.evalRuleBase(ruleBase, X_balanced, y_balanced)
+        ev_object = evr.evalRuleBase(ruleBase, X, y)
         ev_object.add_rule_weights()
 
         score_acc = ev_object.classification_eval()
@@ -113,7 +113,7 @@ try:
     feature_studied = int(sys.argv[6]) - 1
 except:
     print('Using default parameters')
-    n_gen = 1000
+    n_gen = 100
     pop_size = 30
     nRules = 15
     nAnts = 4
