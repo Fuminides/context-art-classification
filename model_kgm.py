828d268c9331867aa29a7f9fb4334aa9cdd6bca7
Javier Fumanal Idocin <javierfumanalidocin@gmail.com>
2023-09-12 15:22:41 +0200

    Update model_kgm.py

--- model_kgm.py
+++ model_kgm.py
@@ -120,10 +120,12 @@ class KGM(nn.Module):
 
             if visual_emb.is_cuda:
                 self.classifier1.cuda()
-                self.classifier2.cuda()
-                self.classifier3.cuda()
-                self.classifier4.cuda()
+
                 self.nodeEmb.cuda()
+                if self.multi_task:
+                    self.classifier2.cuda()
+                    self.classifier3.cuda()
+                    self.classifier4.cuda()
 
         visual_emb = visual_emb.view(visual_emb.size(0), -1)
 
